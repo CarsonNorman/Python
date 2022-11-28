91c1ebf392b096a0603c3bd77737cae74e8bb055
CarsonNorman <95375043+CarsonNorman@users.noreply.github.com>
2022-11-28 12:33:20 -0800

    Create main.py

---
+++ Dictionaries/main.py
@@ -0,0 +1,58 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+print(x)
+students[0]['last_name'] = 'Bryant'
+print(students)
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+z[0]['y'] = 30
+print(z)
+
+def iterateDictionary(some_list):
+    for i in range(len(some_list)):
+        temp = ""
+        for k, v in some_list[i].items():
+            temp += f"{k}, {v} "
+        print(temp)
+
+newStudents = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(newStudents)
+
+def iterateDictionary2(key_name, some_list):
+    for i in range(len(some_list)):
+        print(some_list[i].get(key_name))
+
+newStudents = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary2('first_name', newStudents)
+
+def printInfo(some_list):
+    for k,v in some_list.items():
+        print(k, len(v))
+        for i in range(len(v)):
+            print(v[i])
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
