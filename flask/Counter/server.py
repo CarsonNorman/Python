35f677d18c51984712555422953e125dd50d66d8
CarsonNorman <95375043+CarsonNorman@users.noreply.github.com>
2022-12-02 13:14:54 -0800

    added counter project

---
+++ flask/Counter/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, session, redirect
+app = Flask(__name__)
+app.secret_key = 'Carson'
+
+@app.route('/')
+def render():
+    if 'count' not in session:
+        session['count'] = 0
+        print('added session')
+    session['count']+=1
+    return render_template('index.html', count=session['count'])
+@app.route('/addTwo', methods=['POST'])
+def addTwo():
+    session['count'] +=1
+    return redirect('/')
+
+@app.route('/destroy_session', methods=['POST'])
+def destroy_session():
+    session['count']=0
+    return redirect('/')
+
+
+if __name__=="__main__":    
+    app.run(debug=True)    
--- flask/html_tables/server.py
+++ flask/html_tables/server.py
@@ -4,7 +4,8 @@ users = [
    {'first_name' : 'Michael', 'last_name' : 'Choi'},
    {'first_name' : 'John', 'last_name' : 'Supsupin'},
    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-   {'first_name' : 'KB', 'last_name' : 'Tonel'}
+   {'first_name' : 'KB', 'last_name' : 'Tonel'},
+   {'first_name' : 'Jacob', 'last_name': 'Anile'}
 ]
 
 @app.route('/')
