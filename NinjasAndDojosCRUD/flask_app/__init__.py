37e08676a31e27e5ec23d2e7894748e5a06cdc83
CarsonNorman <95375043+CarsonNorman@users.noreply.github.com>
2022-12-09 15:12:25 -0800

    Made log in and verification project using Flask, Sql, and Bcrypt

--- NinjasAndDojosCRUD/flask_app/__init__.py
+++ NinjasAndDojosCRUD/flask_app/__init__.py
@@ -1,5 +1,3 @@
 from flask import Flask, redirect, request, render_template as render
 app= Flask(__name__, template_folder='static/templates')
 
-from flask_app.controllers.dojos_controller import Dojo
-from flask_app.controllers.ninjas_controller import Ninja
