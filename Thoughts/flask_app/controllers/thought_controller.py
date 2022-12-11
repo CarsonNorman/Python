37ea746b47ba9ad1e368e986008cc0003435f73d
CarsonNorman <95375043+CarsonNorman@users.noreply.github.com>
2022-12-10 16:06:48 -0800

    Created Basic social media app to share thoughts

---
+++ Thoughts/flask_app/controllers/thought_controller.py
@@ -0,0 +1,56 @@
+from flask_app import app
+from flask import Flask, render_template as render, redirect, request, session, flash, url_for
+from flask_app.models.thoughts_model import Thought
+from flask_app.models.login_model import Login
+
+
+@app.route('/dashboard/<int:id>')
+def renderHome(id):
+    if 'id' in session and int(id) == int(session['id']):
+        userData = Login.get_one({'id':id})
+        print('filter', filter)
+        if 'filter' in session:
+            if session['filter'] == 'following':
+                print('Running following filter' )
+                postData = Thought.get_all_following()
+            if session['filter'] == 'own':
+                print('Running own thoughts filter' )
+            postData = Thought.get_user_thoughts({'id':id})
+            if session['filter'] == 'all':
+                print('Running all filter' )
+                postData = Thought.get_all_thoughts()
+        else:
+            print('Running all filter' )
+            postData = Thought.get_all_thoughts()
+        print(postData)
+        return render('dashboard.html', userData = userData, postData=postData)
+    else: 
+        flash('Please Login')
+        return redirect(url_for('landing', err=3))
+
+@app.route('/applyFilter/<int:id>/<filter>')
+def applyFilter(id, filter):
+    session['filter'] = filter
+    return redirect(url_for('renderHome', id=id))
+
+@app.route('/delete/post/<int:id>')
+def deletePost(id):
+    if 'id' in session:
+        Thought.delete({'id':id})
+        return redirect(url_for('renderHome', id=session['id']))
+    return redirect(url_for('landing', err=3))
+
+@app.route('/create/post/<int:id>')
+def renderPost(id):
+    return render('newPost.html', id=id)
+
+@app.route('/create/post/<int:id>/confirm', methods=['POST'])
+def createPost(id):
+    if 'id' in session:
+        post=request.form['post']
+        if post != '':
+            Thought.create({'user_id': id, 'thought': post})
+            return redirect(url_for('renderHome', id=session['id']))
+        flash('Post must not be empty')
+        return redirect(url_for('renderPost', id=id))
+    return redirect(url_for('landing', err=3))
---
+++ Thoughts/flask_app/models/thoughts_model.py
@@ -0,0 +1,49 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import Flask, render_template as render, redirect, request, session, flash, url_for
+
+class Thought:
+    def __init__(self, data):
+        self.name = data['name']
+        self.thought = data['thought']
+        self.user_id = data['user_id']
+    
+    @classmethod 
+    def create(cls, data):
+        query = """
+        INSERT INTO thoughts (thought,user_id)
+        VALUES(%(thought)s, %(user_id)s)
+        """
+        results = connectToMySQL('thoughts').query_db(query, data)
+        return results
+
+    @classmethod
+    def get_user_thoughts(cls, data):
+        print('data', data)
+        query="""
+        SELECT users.name, thoughts.thought, users.id, thoughts.id AS thought_id
+        FROM users JOIN thoughts ON users.id = thoughts.user_id
+        WHERE users.id = %(id)s;
+        """
+        results = connectToMySQL('thoughts').query_db(query, data)
+        print(results)
+        return results
+
+    @classmethod
+    def get_all_thoughts(cls):
+        query="""
+        SELECT users.name, thought, users.id, thoughts.id AS thought_id
+        FROM thoughts JOIN users ON thoughts.user_id = users.id
+        """
+        return connectToMySQL('thoughts').query_db(query)
+    @classmethod
+    def delete(cls, data):
+        query="""
+        DELETE FROM thoughts 
+        WHERE id = %(id)s
+        """
+        results = connectToMySQL('thoughts').query_db(query, data)
+        return results
+
+    @classmethod
+    def get_all_following(cls):
+        return cls.get_all_thoughts()
